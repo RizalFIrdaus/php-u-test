75432d3009fafc672efb95342e6da3e2f1ee7fe3
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-26 21:43:56 +0700

    Counter Test with UnitTest

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Rizalfirdaus\UnitTest;
+
+use PHPUnit\Framework\TestCase;
+
+class CounterTest extends TestCase
+{
+    public function testCounter()
+    {
+        $counter = new Counter();
+        $counter->increment();
+        echo $counter->getCounter() . PHP_EOL;
+    }
+}
