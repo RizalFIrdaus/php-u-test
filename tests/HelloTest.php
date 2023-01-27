31fc8641923cf4a9e5bf26f8f2c73486f52ea88f
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-27 14:28:22 +0700

    Skiped Test

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -8,29 +8,41 @@ class HelloTest extends TestCase
 {
 
     private SayHello $object;
+
+
     public function setUp(): void
     {
         $this->object = new SayHello("Rizal");
     }
-    public function tearDown(): void
-    {
-        echo "Teardown" . PHP_EOL;
-        Self::markTestSkipped("Test Hallo skipped");
-    }
+    // public function tearDown(): void
+    // {
+    //     // Self::markTestSkipped("Test Hallo skipped");
+    //     echo "Teardown" . PHP_EOL;
+    // }
 
 
+    /**
+     * @requires PHP <= 7.2
+     * @requires OS Linux
+     */
     public function testHello()
     {
         $this->object->Hello();
         Self::assertClassHasAttribute("name", sayHello::class);
     }
 
+    /**
+     * @requires PHP <= 7.2
+     * @requires OS Linux
+     */
     public function testException()
     {
         $say = new SayHello("");
         $this->expectException(\Exception::class);
         $say->Hello();
     }
+
+
     public function testExpectOutput()
     {
         $this->object->Hello();
